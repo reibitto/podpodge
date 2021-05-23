a9384713928ddf3dd7ed0a17131f6a1d190a0b9c
reibitto <reibitto@users.noreply.github.com>
2021-05-23 17:40:07 +0900

    Remove unneeded plugin

--- project/Plugins.scala
+++ project/Plugins.scala
@@ -1,8 +1,8 @@
 import sbt._
 
 object Plugins {
+
   lazy val BaseCompilerPlugins = Seq(
-    compilerPlugin("com.olegpy"    %% "better-monadic-for" % "0.3.1"),
-    compilerPlugin("org.typelevel" %% "kind-projector"     % "0.13.0" cross CrossVersion.full)
+    compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
   )
 }
