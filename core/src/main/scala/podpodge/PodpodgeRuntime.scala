2ed8af6df0ec061f9de69fdc72af53d5251aee78
reibitto <reibitto@users.noreply.github.com>
2021-07-12 18:14:14 +0900

    Upgrade Quill and use zio-magic

--- core/src/main/scala/podpodge/PodpodgeRuntime.scala
+++ core/src/main/scala/podpodge/PodpodgeRuntime.scala
@@ -1,18 +1,24 @@
 package podpodge
 
-import io.getquill.context.ZioJdbc.QDataSource
+import io.getquill.context.ZioJdbc.{ DataSourceLayer, QDataSource }
 import podpodge.config.Config
 import sttp.client.httpclient.zio.HttpClientZioBackend
 import zio.internal.Platform
 import zio.logging.{ LogLevel, Logging }
-import zio.{ Runtime, ZEnv }
+import zio.{ Runtime, ZEnv, ZLayer }
 
 trait PodpodgeRuntime extends Runtime[Env] {
   lazy val default: Runtime.Managed[Env] = Runtime.unsafeFromLayer {
-    ZEnv.live >+>
-      (Logging.console(LogLevel.Trace) ++ HttpClientZioBackend
-        .layer() ++ (QDataSource.fromPrefix("ctx") >>> QDataSource.toConnection)) >+>
+    import zio.magic._
+
+    ZLayer.fromMagic[Env](
+      ZEnv.live,
+      Logging.console(LogLevel.Trace),
+      HttpClientZioBackend.layer(),
+      QDataSource.fromPrefix("ctx"),
+      DataSourceLayer.live,
       Config.live
+    )
   }
 
   lazy val environment: Env   = default.environment
